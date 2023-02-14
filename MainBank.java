a259eca951fc319012ffd6c187683692bf115ccb
RRoosimagi <125130906+RRoosimagi@users.noreply.github.com>
2023-02-14 19:14:06 +0200

    Add files via upload

---
+++ MainBank.java
@@ -0,0 +1,8 @@
+package Bank;
+
+public class MainBank {
+    public static void main(String[] args) {
+        Pocket pocket = new Pocket(-3001);
+        System.out.println(pocket.getMoney());
+    }
+}
